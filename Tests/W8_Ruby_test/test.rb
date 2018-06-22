1780deef7a44b23220d7df5ede1d6d0bfe9fbb2b
Claudio Rivera <claudio.rivera.sh@gmail.com>
2018-06-21 20:46:37 -0400

    Fixes and refactoring

--- Tests/W8_Ruby_test/test.rb
+++ Tests/W8_Ruby_test/test.rb
@@ -6,58 +6,65 @@ def menu
     puts '4. Salir'
 end
 
-def aprueba(aprueba_con=5)
-    puts 'Alumnos aprobados'
-    content = File.open("notas.csv", 'r')
-    content.each_line do |line|
-        words = line.split(", ")
-        nombre = words.shift
-        promedio = words.shift
+def menu_option_1
+  content = File.open("alumnos.csv", 'r')
+  content.each_line do |line|
+    words = line.split(", ")
+    nombre_alumno = words.shift
+    file = File.open("notas.csv", 'a')
 
-        if promedio.to_f >= aprueba_con.to_f
-            puts nombre
-        end
+    suma = 0
+    promedio = 0
+    words.each do |nota|
+      nota == 'A' ? (suma += 1) : (suma += nota.to_f)
     end
+    promedio = suma / words.length
+    file.puts "#{nombre_alumno}, #{promedio}"
+    file.close
+  end
+ensure
+  content.close
+end
+
+def menu_option_3
+  menu_option_1
+  promedio = gets.chomp!
+  aprueba(promedio)
+end
+
+def aprueba(aprueba_con=5.0)
+  puts 'Alumnos aprobados'
+  content = File.open("notas.csv", 'r')
+  content.each_line do |line|
+    words = line.split(", ")
+    nombre = words.shift
+    promedio = words.shift
+
+    puts nombre if promedio.to_f >= aprueba_con.to_f
+  end
 end
 
 menu
 option = gets.chomp!
 
 while option != '4'
-    case option
-    when '1'
-        content = File.open("alumnos.csv", 'r')
-        content.each_line do |line|
-            words = line.split(", ")
-            nombre_alumno = words.shift
-            file = File.open("notas.csv", 'a+')
+  case option
+  when '1'
+    menu_option_1
+    menu
 
-            suma = 0
-            promedio = 0
-            inasistencias = 0
-            words.each do |nota|
-                nota == 'A' ? (inasistencias += 1) : (suma += nota.to_f)
-            end
+  when '2'
+    print "\nInasistencias totales: ", File.read("alumnos.csv").scan("A").length, "\n\n"
+    menu
 
-            promedio = suma/(words.length - inasistencias)
-            file.puts "#{nombre_alumno}, #{promedio}"
-        end
-        menu
+  when '3'
+    puts "\n\nCon que nota se aprueba el ramo?"
+    menu_option_3
+    menu
+  else
+    puts 'Selección no se encuentra en el rango'
+    menu
+  end
 
-    when '2'
-        print "\n\nInasistencias totales: ", File.read("alumnos.csv").scan("A").length, "\n"
-        menu
-        
-    when '3'
-        puts "\n\nCon que nota se aprueba el ramo?"
-        promedio = gets.chomp!
-        aprueba(promedio)
-
-        menu
-    else
-        puts 'Selección no se encuentra en el rango'
-        menu
-    end
-
-    option = gets.chomp!
+  option = gets.chomp!
 end
